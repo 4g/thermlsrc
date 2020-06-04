45fbd9efaa59bf2c2785c2095bba6763b7beec58
krishna <sdvk291@gmail.com>
2020-06-04 13:15:10 +0530

    flask restful

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from influxdb import InfluxDBClient
-
+from flask_restful import Api, Resource
 def create_app(config_file_name):
   app = Flask(__name__)
+  api = Api(app)
   app.config.from_object(config_file_name)
-  from app.routes.data_transfer import data_transfer_bp
-  app.register_blueprint(data_transfer_bp)
+  from app.routes.data_transfer import DataTransfer
+  api.add_resource(DataTransfer, '/')
   
   return app
 
--- src/app/routes/data_transfer.py
+++ src/app/routes/data_transfer.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, current_app
 
-data_transfer_bp = Blueprint('data_transfer_bp', __name__)
+# data_transfer_bp = Blueprint('data_transfer_bp', __name__)
 from ..services import *
-@data_transfer_bp.route("/")
-def test():
-  print(dir(current_app.config))
-  print(current_app.config)
-  create_db('YO')
-  return "Yo"
+# @data_transfer_bp.route('/insert',methods=('POST'))
+# def test():
+#   print(dir(current_app.config))
+#   print(current_app.config)
+#   create_db('YO')
+#   return "Yo"
+from flask_restful import Resource
+class DataTransfer(Resource):
+  def get(self):
+    create_db('New')
+    return "Yo"
